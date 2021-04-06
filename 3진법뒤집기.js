d19eb125e5996ca48bfdc13b4ddcc378e692c11c
username <byoungdo95@gmail.com>
2021-04-06 20:11:00 +0900

    2021.04.06 3진법뒤집기

---
+++ 3진법뒤집기.js
@@ -0,0 +1,23 @@
+var c = [];
+function solution(n) {
+	var answer = 0;
+	var a = n.toString(3); //1200
+	var b = a.split(''); //['0','0','2','1']
+	for (var i = 0; i < b.length; i++) {
+		var c = b[i] * Math.pow(3, i);
+		answer += c;
+	}
+	// console.log(a);
+	// console.log(b);
+	// console.log(c);
+	console.log(answer);
+}
+solution(45);
+
+// 숫자 제곱 만드는 방법
+// let a = Math.pow(5, 2);
+// console.log(a);
+
+// 스트링 상태에서 순서를 뒤집는 방법
+// const var1 = 'english123';
+// console.log(var1.split('').reverse().join(''));
---
+++ level1/3진법뒤집기.js
@@ -0,0 +1,13 @@
+function solution(n) {
+	var sam = n.toString(3); // 1200
+	var trans = []
+
+	console.log(trans);
+
+	// return answer;
+}
+solution(45);
+
+1. n을 삼진법 (sam) 으로 바꾸기
+2. var trans = sam 을 쪼개서 순서를 바꿔주기
+3. trans 를 10진법으로 표현하기
