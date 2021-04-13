e9305048dbfc304df38cdcceb1a0b67021751391
username <byoungdo95@gmail.com>
2021-04-13 15:24:40 +0900

    2021.04.13

--- level1/test.js
+++
@@ -1,9 +0,0 @@
-function solution(arr, com) {
-	var answer = [];
-	for (i = 0; i < arr.length; i++) {
-		var a = com[i][0];
-	}
-
-	return answer;
-}
-solution(arr, com);
---
+++ level1/체육복.js
@@ -0,0 +1,4 @@
+function solution(n, lost, reserve) {
+	return answer;
+}
+solution(5, [2, 4], [1, 3, 5]);
