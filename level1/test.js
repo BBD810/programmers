d5ba94a5df9037dd785f44f126293e8808d990d6
username <byoungdo95@gmail.com>
2021-04-18 16:24:09 +0900

    04.18 체육복

---
+++ level1/test.js
@@ -0,0 +1,4 @@
+var a = [1, 2, 3, 4];
+var b = [1, 3, 5];
+
+console.log(a.includes && b.includes);
--- level1/체육복.js
+++ level1/체육복.js
@@ -1,4 +1,35 @@
+// n = 전체 학생의 수
+// lost = 잃어버린 학생의 번호가 담긴 배열
+// reserve = 여벌의 체육복을 가져온 학생들의 번호가 담긴 배열
+
+// 1. 뒷번호의 학생이 앞번호의 학생에게 빌려주는 경우
+// if 잃어버린 학생이  lost에 포함되어 있고 , 뒷번호의 학생이 reserve 에 포함되어 있으면 -> answer에 옮겨주고 lost 삭제 reserve 삭제
+// 출력은 answer.length
+// answer.length
+
+var answer = []; //answer = 체육복을 입을 수 있는 애들
 function solution(n, lost, reserve) {
-	return answer;
+	var cross = [];
+	for (x = 0; x < lost.length; x++) {
+		for (y = 0; y < reserve.length; y++) {
+			if (lost[x] === reserve[y]) {
+				cross.push(lost[x]);
+			}
+		}
+	}
+	// console.log(cross);
+
+	var self = n - lost.length - reserve.length + cross.length;
+	for (var i = 0; i < 10; i++) {
+		//i = 잃어버린 ( 앞)
+		if (lost.includes(i) && reserve.includes(i + 1)) {
+			answer.push(i);
+		} else {
+			// console.log('hi');
+		}
+	}
+	console.log(self);
+	// console.log(answer);
 }
-solution(5, [2, 4], [1, 3, 5]);
+// solution(5, [2, 4], [1, 3, 5]);
+solution(7, [1, 2, 3, 4, 6, 7], [1, 2, 3]);
