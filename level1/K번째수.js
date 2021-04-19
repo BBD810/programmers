e4193a794d826e59c34a60d33b748ddcb2fd3590
username <byoungdo95@gmail.com>
2021-04-19 20:56:12 +0900

    2021.04.19 K번째수

--- level1/K번째수.js
+++ level1/K번째수.js
@@ -1,6 +1,22 @@
-function solution(arr, com) {
-	var answer = [];
+// 2021.04.19 해결
+// 배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬할 때 k 번째 수 구하기
 
+function solution(array, commands) {
+	var answer = [];
+	for (var i = 0; i < commands.length; i++) {
+		var cut = array.slice(commands[i][0] - 1, commands[i][1]);
+		console.log('cut', cut);
+		var sort = cut.sort(function (a, b) {
+			if (a > b) return 1;
+			if (a === b) return 0;
+			if (a < b) return -1;
+		});
+		console.log('sort', sort);
+		var choice = sort[commands[i][2] - 1];
+		answer.push(choice);
+		console.log('answer', answer);
+	}
+	console.log('answer', answer);
 	return answer;
 }
 solution(
@@ -12,11 +28,33 @@ solution(
 	]
 );
 
-// 배열 일부 가져오기 - Slice(a,b)
-// a = 첫번째 수의 인덱스, b = 두번쨰 수의 인덱스 -1
-// const arr = [1,2,3,4,5]
-//  arr.slice(3);    // [4,5]
-//  arr.slice(2,4);  // [3,4]
+// 풀이과정
 
-arr = [1, 5, 2, 6, 3, 7, 4],
-com = [[2, 5, 3],[4, 4, 1],[1, 7, 3]]
+// function solution(array, commands) {
+// 	var answer = [];
+// 	var cut1 = array.slice(commands[0][0] - 1, commands[0][1]); // ==> [5,2,6,3]
+// 	var cut2 = array.slice(commands[1][0] - 1, commands[1][1]); // ==> [6]
+// 	var cut3 = array.slice(commands[2][0] - 1, commands[2][1]); //==> [1,5,2,6,3,7,4]
+// 	// console.log(cut1, cut2, cut3);
+// 	for (var i = 0; i < commands.length; i++) {
+// 		var cut = array.slice(commands[i][0] - 1, commands[i][1]);
+
+// 		// console.log(cut);
+// 	}
+// 	var sort = cut.sort();
+// 	// console.log(sort);
+// 	var choice1 = sort[commands[0][2] - 1];
+// 	var choice2 = sort[commands[1][2] - 1];
+// 	var choice3 = sort[commands[2][2] - 1];
+// 	// console.log(choice1, choice2, choice3);
+// 	answer.push(choice1, choice2, choice3);
+// 	console.log(answer);
+// }
+// solution(
+// 	[1, 5, 2, 6, 3, 7, 4],
+// 	[
+// 		[2, 5, 3],
+// 		[4, 4, 1],
+// 		[1, 7, 3],
+// 	]
+// );
--- level1/test.js
+++ level1/test.js
@@ -1,14 +1,55 @@
-function solution(s) {
-	var a = s.split('');
-	var answer = [];
-	if (a.length % 2 === 0) {
-		answer.push(a[a.length / 2 - 1]);
-		answer.push(a[a.length / 2]);
-	} else {
-		answer.push(a[Math.floor(a.length / 2)]);
-	}
-	return console.log(answer.join(''));
-}
+// function solution(array, commands) {
+// 	var answer = [];
+// 	var cut1 = array.slice(commands[0][0] - 1, commands[0][1]); // ==> [5,2,6,3]
+// 	var cut2 = array.slice(commands[1][0] - 1, commands[1][1]); // ==> [6]
+// 	var cut3 = array.slice(commands[2][0] - 1, commands[2][1]); //==> [1,5,2,6,3,7,4]
+// 	// console.log(cut1, cut2, cut3);
+// 	for (var i = 0; i < commands.length; i++) {
+// 		var cut = array.slice(commands[i][0] - 1, commands[i][1]);
 
-solution('abce');
-solution('abcdefg');
+// 		// console.log(cut);
+// 	}
+// 	var sort = cut.sort();
+// 	// console.log(sort);
+// 	var choice1 = sort[commands[0][2] - 1];
+// 	var choice2 = sort[commands[1][2] - 1];
+// 	var choice3 = sort[commands[2][2] - 1];
+// 	// console.log(choice1, choice2, choice3);
+// 	answer.push(choice1, choice2, choice3);
+// 	console.log(answer);
+// }
+// solution(
+// 	[1, 5, 2, 6, 3, 7, 4],
+// 	[
+// 		[2, 5, 3],
+// 		[4, 4, 1],
+// 		[1, 7, 3],
+// 	]
+// );
+
+function solution(array, commands) {
+	var answer = [];
+	for (var i = 0; i < commands.length; i++) {
+		var cut = array.slice(commands[i][0] - 1, commands[i][1]);
+		console.log('cut', cut);
+		var sort = cut.sort(function (a, b) {
+			if (a > b) return 1;
+			if (a === b) return 0;
+			if (a < b) return -1;
+		});
+		console.log('sort', sort);
+		var choice = sort[commands[i][2] - 1];
+		answer.push(choice);
+		console.log('answer', answer);
+	}
+	console.log('answer', answer);
+	return answer;
+}
+solution(
+	[1, 5, 2, 6, 3, 7, 4],
+	[
+		[2, 5, 3],
+		[4, 4, 1],
+		[1, 7, 3],
+	]
+);
