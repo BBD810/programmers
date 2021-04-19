ba6370ba1452b1407e1f329239ec1d1ad8331c16
username <byoungdo95@gmail.com>
2021-04-20 00:16:36 +0900

    예산

---
+++ level1/예산.js
@@ -0,0 +1,8 @@
+function solution(d, budget) {
+	var answer = 0;
+	d.sort();
+	console.log(d);
+	for (var i = 0; budget === 0; i++) {}
+}
+solution([1, 3, 2, 5, 4], 9);
+// solution([2, 2, 3, 3], 4);
