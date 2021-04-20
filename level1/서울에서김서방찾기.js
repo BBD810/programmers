433426a31b006ee86fa5b14ddd2225444b5c4a23
username <byoungdo95@gmail.com>
2021-04-20 21:12:18 +0900

    서울에서김서방찾기

---
+++ level1/서울에서김서방찾기.js
@@ -0,0 +1,11 @@
+// 2021.04.20 해결 
+function solution(seoul) {
+	var answer = ' ';
+	for (var i = 0; i < seoul.length; i++) {
+		if (seoul[i] === 'Kim') {
+			answer = '김서방은 ' + i + '에 있다';
+		}
+	}
+	return answer;
+}
+solution(['Jane ', 'Kim']);
