48da2cf4b520daf0a407ef7ac1e39cb86014b3ce
username <byoungdo95@gmail.com>
2021-04-20 20:20:00 +0900

    2021.04.20 예산

--- level1/예산.js
+++ level1/예산.js
@@ -12,3 +12,19 @@ function solution(d, budget) {
 	return answer;
 }
 solution([1, 3, 2, 5, 4], 9);
+
+// 최도영코드
+// function solution(d, budget) {
+// 	var answer = 0;
+// 	var count = 0;
+// 	d.sort((a, b) => a - b);
+
+// 	for (var i = 0; i < d.length; i++) {
+// 		if (d[i] <= budget) {
+// 			budget -= d[i];
+// 			count++;
+// 		}
+// 	}
+
+// 	return count;
+// }
