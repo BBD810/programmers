aff87c6842ecb302b12651a5372988ee2c872a84
username <byoungdo95@gmail.com>
2021-04-21 21:46:25 +0900

    짝수와홀수

---
+++ level1/짝수와홀수.js
@@ -0,0 +1,12 @@
+// 2021.04.21 해결
+function solution(num) {
+	var answer = '';
+	if (num % 2 === 0) {
+		answer = 'Even';
+	} else {
+		answer = 'Odd';
+	}
+	return answer;
+}
+solution(3);
+solution(4);
