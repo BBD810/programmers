62843b814f4fe3d0d52a98c44f3a233d78c7f01d
username <byoungdo95@gmail.com>
2021-04-21 21:11:11 +0900

    자연수뒤집어배열로만들기

--- level1/자연수뒤집어배열로만들기.js
+++ level1/자연수뒤집어배열로만들기.js
@@ -1,6 +1,14 @@
+// 2021.04.21 해결
 function solution(n) {
 	var answer = '';
-	var arr = n.split('');
-	console.log(arr);
+	var result = [];
+	var a = String(n);
+	a = a.split('');
+	a.reverse();
+
+	for (var i = 0; i < a.length; i++) {
+		a[i] = parseInt(a[i]);
+	}
+	return a;
 }
 solution(12345);
