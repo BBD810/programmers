77b5e79a9eb0327f3be68eecca27e4fa90f71eed
username <byoungdo95@gmail.com>
2021-04-23 12:58:41 +0900

    최대공약수와최소공배수

--- level1/test.js
+++ level1/test.js
@@ -1,5 +1,30 @@
-var a = ['a', 'b', 'c', 'd', 'e', 'f'];
-var b = ['c', 'd', 'e', 'f', 'g'];
-if (a.includes(b[0])) {
-	console.log('YES');
+// 유클리드 호제법 ( GCD 구하기 )
+//  a를 b로 나눈 나머지를 r이라고 했을 때
+// GCD(a,b) = GCD(b,r)
+// 이때 r이 0이라면 그때의 b가 최대공약수
+
+function solution(n, m) {
+	var big;
+	var small;
+	if (n >= m) {
+		n = big;
+		m = small;
+	} else {
+		n = small;
+		m = big;
+	}
+	function getGCD(small, big) {
+		var r;
+		while (r !== 0) {
+			r = big % small; //r=8
+			big = small; //
+			if (r !== 0) {
+				small = r;
+			}
+		}
+		var GCD = small;
+		console.log(GCD);
+	}
+	getGCD(small, big);
 }
+solution(15, 16);
