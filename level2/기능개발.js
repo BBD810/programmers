64b62968fa3895ec6042bbb25dfd722d975a883a
username <byoungdo95@gmail.com>
2021-04-26 20:24:34 +0900

    2021.04.26 기능개발

--- level2/기능개발.js
+++ level2/기능개발.js
@@ -1,28 +1,32 @@
+// 2021.04.26 해결
+
 function solution(progresses, speeds) {
 	var answer = [];
-	var result = [];
+	var count = 0;
 	while (progresses.length !== 0) {
 		for (var i = 0; i < progresses.length; i++) {
 			progresses[i] += speeds[i];
 		}
 		// console.log('progresses =', progresses);
 
-		for (var j = 0; j < progresses.length; j++) {
-			if (progresses[0] >= 100) {
-				result.push(progresses.shift());
-				speeds.shift();
-				j = 0;
-			}
-			if (result.length !== 0) {
-				answer.push(result.length);
-			}
+		while (progresses[0] >= 100) {
+			progresses.shift();
+			speeds.shift();
+			count++;
 		}
+		if (count === 0) {
+			continue;
+		} else {
+			answer.push(count);
+			count = 0;
+		}
+
 		console.log('progresses =', progresses, 'speeds =', speeds);
 		console.log('answer =', answer);
 	}
 }
-solution([93, 30, 55], [1, 30, 5]);
-// solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]);
+// solution([93, 30, 55], [1, 30, 5]);
+solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]);
 
 // progresses의 각 요소에 반복문이 실행될 때마다 speeds를 더함, count++
 // progresses의 각 요소들이 100이 넘어가면 true로 바꿔줌
