541215a657d98c84526ffb8fa3425be17d3a5924
username <byoungdo95@gmail.com>
2021-04-26 22:23:13 +0900

    프린터

---
+++ level2/프린터.js
@@ -0,0 +1,8 @@
+function solution(priorities, location) {
+	var answer = 0;
+	var queue = priorities.shift();
+	var max = Math.max.apply(null, priorities);
+	while (priorities.length !== 0) {}
+}
+// solution([2, 1, 3, 2], 2);
+solution([1, 1, 9, 1, 1, 1], 0);
--- 프린터.js
+++
@@ -1,5 +0,0 @@
-function solution(priorities, location) {
-	var answer = 0;
-}
-solution([2, 1, 3, 2], 2);
-// solution([1, 1, 9, 1, 1, 1], 0);
